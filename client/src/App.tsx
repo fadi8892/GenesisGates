a03e422adc268980ff282f332379d329df5462b6
fadi8892 <fadi8892@gmail.com>
2025-08-24 13:09:40 -0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { migrateToIPFS } from './migrate';
 import LandingPage from './pages/LandingPage';
 import Dashboard from './pages/Dashboard';
 
 function App() {
+  useEffect(() => {
+    migrateToIPFS();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
