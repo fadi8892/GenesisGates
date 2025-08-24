ad7f5614b6af1bad7dceec55a1349432fdc4eed3
fadi8892 <fadi8892@gmail.com>
2025-08-24 14:17:06 -0700

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{ts,tsx}'],
-  theme: { extend: {} },
-  plugins: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        gg: {
+          primary: '#5850EC'
+        }
+      }
+    }
+  },
+  plugins: []
 };
