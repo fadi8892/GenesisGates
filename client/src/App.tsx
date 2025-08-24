0a062901c2ea618ff25d8d5ac905a50aa911caeb
fadi8892 <fadi8892@gmail.com>
2025-08-24 12:57:33 -0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Dashboard from './pages/Dashboard';
