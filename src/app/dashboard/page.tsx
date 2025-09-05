49e5ac837e17274c2a2e9393772e8beebb3b16fc
fadi8892 <fadi8892@gmail.com>
2025-09-05 08:03:59 -0700

    Remove

--- src/app/dashboard/page.tsx
+++
@@ -1,6 +0,0 @@
-import Client from "./client";
-
-export const dynamic = "force-dynamic";
-export default function DashPage() {
-  return <Client />;
-}
--- src/app/tree/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-import TreeClient from "./tree-client";
-
-export const dynamic = "force-dynamic";
-export default function TreePage({ params }: { params: { id: string } }) {
-  return <TreeClient treeId={params.id} />;
-}
