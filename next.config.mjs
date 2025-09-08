d97ee631c2ef61d46d069dedebec37538e80e75e
fadi8892 <fadi8892@gmail.com>
2025-09-08 11:30:39 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,9 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Server build for Vercel (no static export)
-  reactStrictMode: true,
-  images: {
-    unoptimized: false,
-  },
-  // plugins: [{ name: 'next' }] // Next may auto-add this; harmless
-};
-
-module.exports = {
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
 };
+
+export default nextConfig;
