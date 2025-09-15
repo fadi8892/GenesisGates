3da3c202012b47466f931026c28007d4fd0d5419
fadi8892 <fadi8892@gmail.com>
2025-09-14 23:44:08 -0700

    Delete next.config.cjs

--- next.config.cjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  trailingSlash: true,
-  images: {
-    unoptimized: true, // Disables image optimization, common for server-rendered apps or external image hosting
-  },
-};
